798e8b3d71c0855906040366e5f25d0b131d4703
selaw1 <88832989+selaw1@users.noreply.github.com>
2021-09-04 15:32:18 +0300

    make it better

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p%*7a)d$t3=f7ua41$1-t)#_3=fa$(+i+$&h91m1a$8@cb+_l_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
